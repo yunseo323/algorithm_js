6a1a084c3094795ba638e7bf2d31bd4211781f71
yunseo323 <jokani3244833@gmail.com>
2022-03-24 23:57:43 +0900

    section3

--- codeFile/section3/3-4.js
+++ codeFile/section3/3-4.js
@@ -1,13 +1,51 @@
 function solution(str,word){
-    let answer=[], index=[];
-    answer = str.split('');
-    for(let i=0;i<answer.length;i++){
-        if(answer[i]===word) index.push(i);
+    /*  매 문자마다 왼쪽 오른쪽 거리 비교로 구현함..
+    let s=[], index=[];
+    const len = str.length;
+    s = str.split('');
+    for(i=0;i<len;i++){
+        let j=0,m=0,flag=0;
+        while(i+j>=0&&i+j<len){
+            if(s[i+j]===word){
+                flag=1; //같은걸 찾음
+                cnt=j;
+                break;
+            }
+            j++;
+        }
+        j=0;
+        while(i+j>=0&&i+j<len){
+            j--;
+            if(s[i+j]===word){
+                flag=1; //같은걸 찾음
+                if(cnt>-j) cnt = -j;
+                break;
+            }
+        }
+        index.push(cnt);
     }
-    for(let i=0;i<answer.length;i++){
+    return(index);
+    */
+
+    //배열을 총 두번 도는 것으로 끝낼 수 있음
+    let answer=[],s=[];
+    const len = str.length;
+    s = str.split('');
+    let cnt = Number.MAX_SAFE_INTEGER;
+    for(let i=0;i<len;i++){ //오른쪽으로 이동하면서 왼쪽 단어와의 거리 계산
         
+        if(s[i]===word)cnt=0;
+        else cnt++;
+        answer.push(cnt);
     }
-    console.log(index);
+    cnt = Number.MAX_SAFE_INTEGER;
+    for(let i=len-1;i>=0;i--){ //왼쪽으로 이동하면서 오른쪽 단어와의 거리 계산
+        if(s[i]===word)cnt=0;
+        else cnt++;
+        if(answer[i]>cnt) answer[i]=cnt; //기존 answer 배열과 비교해 더 작으면 바꿈
+    }
+    return(answer);
+
 }
 
 let str="teachermode";
