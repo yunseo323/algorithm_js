b1b53dfce72a7e7dc4be3fe3ead88c3c5de1fa9e
yunseo323 <jokani3244833@gmail.com>
2022-04-24 00:44:32 +0900

    section7

--- codeFile/section7/7-11.js
+++ codeFile/section7/7-11.js
@@ -1,6 +1,36 @@
+function count(arr, cap){ //답이 유효한지 검사하는 함수가 결정 알고리즘에서 제일 중요함
+    let cnt=1,sum=0;
+    for(let x of arr){
+        if(sum+x>cap){
+            cnt++;
+            sum=x;
+        }
+        else sum+=x;
+    }
+    return cnt; //유효한 dvd 개수 리턴
+}
 function solution(M,arr){
-    
+    let answer;
+    //순서가 그대로 유지되어야 하므로 정렬 x
+    let lt = Math.max(...arr); //전개연산자를 써서 인자를 넘겨야 함
+    let rt= arr.reduce((a,b)=>a+b,0);
+    while(lt<=rt){
+        let mid=parseInt((lt+rt)/2);
+        if(count(arr,mid)<=M){
+            answer=mid;
+            rt=mid-1;
+        }
+        else lt=mid+1;
+    }
+    return answer;
 }
 
 let arr=[1, 2, 3, 4, 5, 6, 7, 8, 9];
 console.log(solution(3, arr));
+/*
+DVD 가장 짧은 용량은 가장 큰 용량의 노래이다 -> 예시에서 9
+용량의 범위는 가장 큰 용량의 노래 ~ 노래들의 합
+-> 이진탐색을 사용하자
+dvd 용량의 중간값을 구한다
+=> 이 과정을 범위를 좁혀 반복한다
+*/
--- codeFile/section7/7-12.js
+++ codeFile/section7/7-12.js
@@ -1,4 +1,31 @@
-function solution(arr){
-
+function count(arr,mid){ //말 사이의 최대 거리를 기준으로 총 몇마리를 둘 수 있는지
+    let cnt=1, ep=arr[0]; //endpoint(말을 놓는 자리)
+    for(let i=1;i<arr.length;i++){
+        if(arr[i]-ep>=mid){
+            cnt++;
+            ep=arr[i];
+        }
+    }
+    return cnt;
+}
+function solution(N,arr){
+    let answer, cnt = 0;
+    arr.sort((a,b)=>a-b);//오름차순 정렬
+    let lt = arr[0], rt = arr[arr.length-1];
+    while(lt<=rt){
+        let mid=parseInt((lt+rt)/2);// mid: 말 사이의 최대 거리
+        if(count(arr,mid)>=N){
+            answer=mid;
+            lt=mid+1;
+        }
+        else rt=mid-1;
+    }
+    return answer;
 }
 
+let arr=[1, 2, 8, 4, 9];
+console.log(solution(3, arr));
+/*
+결정알고리즘 꼴을 외우기
+mid를 문제에서 어떻게 이해하고 놓을건지가 관건
+*/
