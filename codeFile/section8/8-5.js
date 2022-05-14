ba654cdbb81d0d98c66754006edb71871140a17c
yunseo323 <jokani3244833@gmail.com>
2022-05-14 23:55:46 +0900

    .

--- codeFile/section8/8-5.js
+++ codeFile/section8/8-5.js
@@ -1,14 +1,17 @@
 function solution(arr){
-    let answer=[];
-    let check=Array.from({length:arr.length+1},()=>0);
+    let answer = "YES";
+    let flag;
+    let l = arr.length;
+    let check=Array.from({length:l},()=>0);
     function DFS(v){
         //if-else
-        if(v===n+1){ //종착점
-            let tmp="";
-            for(let i=1;i<=n;i++){
-                if(check[i]===1) tmp+=i+" ";
+        if(v===l){ //종착점
+            let sum0=0,sum1=0;
+            for(let i=0;i<l;i++){
+                if(check[i]===0) sum0+=arr[i];
+                else sum1+=arr[i];
             } 
-            if(tmp.length>0)answer.push(tmp.trim());
+            if(sum0===sum1)flag=1; //yes
         }
         else{
             check[v]=1;
@@ -17,9 +20,13 @@ function solution(arr){
             DFS(v+1);
         }
     }
-    DFS(1);
+    DFS(0);
+    if(flag===0)answer="NO";
     return answer;
 }
 
-let arr=[1,3,5,6,7,10];
+//let arr=[1,3,5,6,7,10];
+let arr=[1,2,4];
 console.log(solution(arr));
+
+//다시 풀기
